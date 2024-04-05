7a3113edf3cd96c3eeb423c167775922862d882d
Soumyadip Paul <161182656+Soumyadip-2005@users.noreply.github.com>
2024-04-05 23:45:11 +0530

    Add files via upload

---
+++ comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1); //true
+// console.log("02">1); // true
+
+
+// console.log(null>0);    //false
+// console.log(null==0);   // false    || The reason is that an equality check ==and comparison > < >= <= work differently.|| 
+// console.log(null>=0);   // true     || Comparisons convert null to a number , treating it as 0. That's why null>=0 is true  & null>0 is false.
+
+// console.log(undefined==0);
+
+// "==="
+
+console.log("2"===2);  // false
