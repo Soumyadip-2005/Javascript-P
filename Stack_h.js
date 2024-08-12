1ce7748dbbd5ad1bb00451a73e95f234a8e3498e
Soumyadip Paul <161182656+Soumyadip-2005@users.noreply.github.com>
2024-08-12 04:00:13 +0000

    Stack_h.js

---
+++ Stack_h.js
@@ -0,0 +1,19 @@
+// Stack(primitive) & Heap (Non-primitive)
+let myName ="SoumyadipPaul"
+let anothername = myName
+anothername = "JeetPaul"
+console.log(anothername);
+console.log(myName);
+
+
+let userOne = {
+    email : "user@gmail.com",
+    upi :"user123@ybl"
+}
+
+let userTwo = userOne 
+
+userTwo.email = "xyz123@gmail.com"
+console.log(userOne.email)
+console.log(userTwo.email)
+
