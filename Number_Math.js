a9a5a29e975199ba15c75aef9e42f5ea764f59b2
Soumyadip Paul <161182656+Soumyadip-2005@users.noreply.github.com>
2024-08-12 18:22:16 +0000

    number_Math.js

---
+++ Number_Math.js
@@ -0,0 +1,29 @@
+const score =  400 
+console.log(score);
+
+const balance = new Number(5192.5839)
+console.log(balance);
+
+
+console.log(balance.toString());
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2))
+
+console.log(balance.toPrecision(3))
+
+// ++++++++++++++++++++++ Math ++++++++++++++++++++++
+
+console.log(Math.abs(-9));
+console.log(Math.round(10.98));
+console.log(Math.ceil(6.1));
+console.log(Math.floor(8.9));
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+
+const max = 20 
+const min = 10 
+
+console.log(Math.floor(Math.random() * ( max - min + 1) + min))
