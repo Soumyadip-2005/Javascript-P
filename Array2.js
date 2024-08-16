4c3529952f5b353ddc76d3e0f575caa0e83f11d1
Soumyadip Paul <161182656+Soumyadip-2005@users.noreply.github.com>
2024-08-16 19:17:08 +0000

    Array2.js

---
+++ Array2.js
@@ -0,0 +1,26 @@
+const marvel_heros = ["thor" , "spiderman" , "ironman"]
+const dcHeros = ["flash" , "superman" , "batman"]
+
+// 
+
+
+//  We have another method to concat the following two arrays , Spread ,method
+// 
+
+
+//  const anotherArray = [1,2,3,[4,5,6],7 , [8,9,[11,22,33]] , 67]
+//  const real_anotherArray = anotherArray.flat(Infinity)
+
+// 
+
+// console.log(Array.isArray("Soumyadip"));
+// console.log(Array.from("Soumyadip"));
+
+// console.log(Array.from({name:"Soumyadip"}))  // Interesting Case 
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
+
+ 
