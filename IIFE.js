6c84117dddadd9c8b2802f304312976c3c7f3ede
Soumyadip Paul <161182656+Soumyadip-2005@users.noreply.github.com>
2024-08-20 20:01:07 +0000

    IIFE.js

---
+++ IIFE.js
@@ -0,0 +1,16 @@
+// IIFE : Stands for immediately invoked function expression. (pronounced iffy )
+
+//  It is a function that is called immediately after it is defined.
+
+// [Many times problems arise due to pollution of the global scope. To remove the pollution of the global scope,   
+// it is necessary to use ]
+(function chai(){
+    // named iffy
+    console.log(`Database Connected`);
+    
+})() ;
+
+( (name) => {
+      console.log(`Database connected Two ${name}`);
+    //    un-named iffy
+} ) ("Soumyadip")
