3080d0f5ec7393cddace3287d306982712b01079
Soumyadip Paul <161182656+Soumyadip-2005@users.noreply.github.com>
2024-08-23 21:24:05 +0000

    Switch.js

---
+++ Switch.js
@@ -0,0 +1,34 @@
+// switch (key) {
+//     case value:
+        
+//         break;   Syntax
+
+//     default:
+//         break;
+// }
+
+const month = 9
+switch (month) {
+    case 1:
+        console.log("January");
+        
+        break;
+    case 2:
+        console.log("Febuary");
+        
+        break;
+    case 3:
+        console.log("March");
+        
+        break;
+    case 4:
+        console.log("April");
+        
+        break;
+    
+
+    default:
+        console.log("Default Case match");
+        
+        break;
+}
